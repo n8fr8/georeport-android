c220c8412f438af4757d15b4b2a01fc4d460da5d
Nathan Freitas <nathan@freitas.net>
2009-03-03 09:43:50 -0500

    new splash screen graphic removed unused constants

--- src/com/openideals/inaugreport/InaugReportConstants.java
+++ src/com/openideals/inaugreport/InaugReportConstants.java
@@ -6,7 +6,5 @@ public interface InaugReportConstants {
 	
 	public final static String REPORTS_URL = "http://inaug09reports.twittervision.com/reports.android";
 	
-	public final static String REPORT_WEB_URL = "http://inaugurationreport.com";
-	
 	public final static String REPORT_URL = "http://tvr.openideals.com:8080/ir/android.html";
 }
