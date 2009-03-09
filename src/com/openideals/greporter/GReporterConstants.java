535baea4b0e79a1237f95ef0c8b147a3b12d9aa8
Nathan Freitas <nathan@freitas.net>
2009-03-08 21:45:51 -0400

    cleaning up the code...

--- src/com/openideals/greporter/GReporterConstants.java
+++
@@ -1,9 +0,0 @@
-package com.openideals.greporter;
-
-public interface GReporterConstants {
-
-	
-	public final static String REPORT_SUBMIT_URL = "http://inaug09reports.twittervision.com/reports.android";
-	
-	public final static String REPORTS_VIEW_URL = "http://tvr.openideals.com:8080/ir/android.html";
-}
